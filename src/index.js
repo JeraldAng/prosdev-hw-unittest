08f84ce2d11dcaa11a715413e3f2c1127949fc91
JeraldAng <jeraldedricang@gmail.com>
2020-07-21 13:15:38 +0800

    updated index.js and index.test.js with screenshot

--- src/index.js
+++ src/index.js
@@ -1,6 +1,25 @@
 const functions = {
   gradeClassifier: (grade) => {
     // TODO: Write code here
+	if (typeof grade !== 'string' && grade !== null){
+		grade = grade.toFixed(2);
+		  if (grade >= 95 && grade <= 100)
+			return "O - Outstanding";
+		  else if (grade >= 90 && grade <= 94.99)
+			return "V - Very Good";
+		  else if (grade >= 85 && grade <= 89.99)
+			return "G - Good";
+		  else if (grade >= 80 && grade <= 84.99)
+			return "S - Satisfactory";
+		  else if (grade >= 75 && grade <= 79.99)
+			return "N - Needs Improvement";
+		  else if (grade >= 0 && grade <= 74.99)
+			return "D - Did not Meet Expectation";
+		  else
+			return "Invalid Grade";
+	}
+	else	
+		return "Invalid Grade";
   }
 }
 
--- src/index.test.js
+++ src/index.test.js
@@ -2,11 +2,135 @@ const { gradeClassifier } = require('./index');
 
 // TODO: Write unit tests here
 describe('Scenario', () => {
-  it('When ..., then ...', () => {
-    // Arrange
+  test('For value O', () => {
+	  // Arrange
+	  const grade = 95;
 
-    // Act
+	  // Act
+	  const result = gradeClassifier(grade);
+	  
+	  // Assert
+	  expect(result).toEqual("O - Outstanding");	
+  })
+  
+  test('For value V', () => {
+	  // Arrange
+	  const grade = 93;
 
-    // Assert
-  });
+	  // Act
+	  const result = gradeClassifier(grade);
+	  
+	  // Assert
+	  expect(result).toEqual("V - Very Good");	
+  })
+  
+  test('For value G', () => {
+	  // Arrange
+	  const grade = 86;
+
+	  // Act
+	  const result = gradeClassifier(grade);
+	  
+	  // Assert
+	  expect(result).toEqual("G - Good");	  
+  })
+  
+  test('For value S', () => {
+	  // Arrange
+	  const grade = 81;
+
+	  // Act
+	  const result = gradeClassifier(grade);
+	  
+	  // Assert
+	  expect(result).toEqual("S - Satisfactory");	
+  })
+  
+  test('For value N', () => {
+	  // Arrange
+	  const grade = 76;
+
+	  // Act
+	  const result = gradeClassifier(grade);
+	  
+	  // Assert
+	  expect(result).toEqual("N - Needs Improvement");	
+  })
+  
+  test('For value D', () => {
+	  // Arrange
+	  const grade = 70;
+
+	  // Act
+	  const result = gradeClassifier(grade);
+	  
+	  // Assert
+	  expect(result).toEqual("D - Did not Meet Expectation");	
+  })
+  
+  test('For equal value in edge of range', () => {
+	  // Arrange
+	  const grade = 74.99;
+
+	  // Act
+	  const result = gradeClassifier(grade);
+	  
+	  // Assert
+	  expect(result).toEqual("D - Did not Meet Expectation");	
+  })
+  
+  test('For more than 2 decimal places', () => {
+	  // Arrange
+	  const grade = 89.9912;
+
+	  // Act
+	  const result = gradeClassifier(grade);
+	  
+	  // Assert
+	  expect(result).toEqual("G - Good");	
+  })
+  
+  test('For invalid negative', () => {
+	  // Arrange
+	  const grade = -22;
+
+	  // Act
+	  const result = gradeClassifier(grade);
+	  
+	  // Assert
+	  expect(result).toEqual("Invalid Grade");	
+  })
+  
+  test('For invalid overflow', () => {
+	  // Arrange
+	  const grade = 202;
+
+	  // Act
+	  const result = gradeClassifier(grade);
+	  
+	  // Assert
+	  expect(result).toEqual("Invalid Grade");	
+  })
+  
+  test('For non-numerical inputs', () => {
+	  // Arrange
+	  const grade = "seventy-eight";
+
+	  // Act
+	  const result = gradeClassifier(grade);
+	  
+	  // Assert
+	  expect(result).toEqual("Invalid Grade");	
+  })
+  
+  test('For null / blank value', () => {
+	  // Arrange
+	  const grade = null;
+
+	  // Act
+	  const result = gradeClassifier(grade);
+	  
+	  // Assert
+	  expect(result).toEqual("Invalid Grade");	
+  })
 });
